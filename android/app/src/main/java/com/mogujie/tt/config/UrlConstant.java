412483d05731aec5dae39498050955ec7b6351b0
zhujian <zhujian@mogujie.com>
2015-07-15 23:02:40 +0800

    zhujian:领导顺序改

--- android/app/src/main/java/com/mogujie/tt/config/UrlConstant.java
+++ android/app/src/main/java/com/mogujie/tt/config/UrlConstant.java
@@ -13,6 +13,6 @@ public class UrlConstant {
     // access 地址
     public final static String ACCESS_MSG_ADDRESS = "http://120.26.38.219/msg_server?devices=android";
 
-    public final static String[] LEADER_ARR = {"赵骏","宣祎扬","沈仕飞","葛文荣","孟涛","董威","魏东"};
+    public final static String[] LEADER_ARR = {"赵骏","宣祎扬","沈仕飞","孟涛","董威","魏东","黄涛","郭立峰","应国校"};
 
 }
